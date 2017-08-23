9f2efee67fd8eea6d1305f0677b8d4f6caa408f9
ddemingjr22 <ddemingjr22@yahoo.com>
2017-08-22 20:50:08 -0400

    worked on practice problems

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -10,6 +10,84 @@ namespace Strings
     {
         static void Main(string[] args)
         {
+            //string firstName = "Dana";
+            //string lastName = "Deming";
+            //string fullname = firstName + " " + lastName;
+            //string fullName = string.Concat(firstName + " " + lastName);
+            ////Console.WriteLine(firstName.Length);
+            ////Console.WriteLine(lastName.Length);
+            //////Console.WriteLine(firstName[0]);
+            ////Console.WriteLine(firstName[firstName.Length]);
+            //Console.WriteLine(fullname);
+            //Console.WriteLine(firstName.Equals(lastName));
+            //Console.WriteLine(firstName == lastName);
+            //Console.WriteLine("My Favorite book is the \"Notebook\"");
+
+            //string nameFirst = "Dana";
+            //string nameLast = "Deming";
+            //string nameFull = nameFirst + " " + nameLast;
+            //Console.WriteLine(nameFull.LastIndexOf('D'));
+
+            //Console.WriteLine(nameFull.IndexOf('a'));
+
+            //Console.WriteLine("Enter your first name");
+            //string firstNameAgain = Console.ReadLine();
+            //Console.WriteLine("Enter your last name");
+            //string lastNameAgain = Console.ReadLine();
+            //if(firstNameAgain.Length == lastNameAgain.Length)
+            //{
+            //    Console.WriteLine("Samsies");
+            //}
+            //else if(firstNameAgain.Length > lastNameAgain.Length)
+            //{
+            //    Console.WriteLine("First is longer");
+            //}
+            //else if (firstNameAgain.Length < lastNameAgain.Length)
+            //{
+            //    Console.WriteLine("Last is longer");
+            //}
+
+            //string tab = "\tab";
+            //string backspace = "'\'";
+            //string bellSound = "\a";
+
+            //Console.WriteLine("This is a tab \t.\nThis is a backspace \\.\nAnd this is a bell sound \a");
+
+            //string person = "Mark";
+            //string nextPerson = "Charlie";
+            //if (person == nextPerson)
+            //{
+            //    Console.WriteLine("The names are the same");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The names are different");
+            //}
+
+            //if (person.Equals(nextPerson))
+            //{
+            //    Console.WriteLine("The names are the same");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The names are different");
+            //}
+
+            Console.WriteLine("Please enter your first name");
+            string firstNameAgain = Console.ReadLine();
+            Console.WriteLine("Please enter your last name");
+            string lastNameAgain = Console.ReadLine();
+            string fullname = "firstNameAgain" +" " + "lastNameAgain";
+            Console.WriteLine(fullname);
+
+
+
+
+
+
+
+
+
         }
     }
 }
